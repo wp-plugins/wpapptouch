cd5dd6911614d036d7e620d4e330b12c9e4f2b55
onigetoc <onigetoc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-17 19:13:52 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wpapptouch/tags/0.5@559484 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp_apptouch.php
+++ wp_apptouch.php
@@ -5,7 +5,7 @@ Plugin URI: http://wpapptouch.com/
 Author: Gino Cote
 Author URI: http://wpapptouch.com/
 Description: WP-appTouch - Web application for Wordpress who look like native applications.
-Version: 0.7
+Version: 0.5
 */
 
 // Activate theme switching. 
